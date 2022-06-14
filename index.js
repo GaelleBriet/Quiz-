b3abb39300ae45370c0b16fd04fc4c3ead38eeff
Gaelle Briet <gaelle.briet@protonmail.com>
2022-06-14 21:47:59 +0200

    suite

--- index.js
+++ index.js
@@ -24,29 +24,60 @@ btn.addEventListener("click", (e) => {
   });
 });
 
+// ************************************************************************** //
 // bonnes réponses
-let rep1 = document.querySelectorAll(".response-choix");
-console.log(rep1);
+let rep1 = document.getElementById("res1-1");
+let rep2 = document.getElementById("res2-2");
+let label1 = document.getElementById("label1");
+let lettre = document.getElementById("choix1");
 
-function isTrue() {
-  var valeur = "";
-  for (let i = 0; i < inputList.length; i++) {
-    if (inputList[i].checked) {
-      valeur = inputList[i].value;
-    }
-  }
-  alert(valeur + " a été sélectionné");
+// ajouter .correctAnswer ou .wrongAnswer suivant la réponse
+// ajouter background color a label span suivant la réponse (rouge / vert)
+if (rep1.checked) {
+  label1.classList.add("correctAnswer");
+  lettre.classList.add("correctSpan");
 }
 
-// a faire
-// recuperer la reponse de l'input radio
+// ******************************************************************************* //
+// chercher du côté de .value
+
+// if (rep1.checked) {
+//   label1.classList.add("correctAnswer");
+//   lettre.classList.add("correctSpan");
+// } else {
+//   label1.classList.add("correctAnswer");
+//   lettre.classList.add("correctSpan");
+//   document
+//     .querySelector("input[type='radio']:checked")
+//     .classList.add("wrongAnswer");
+// }
+
+// quand bonne réponse : seulement vert
+// quand réponse fausse : vert + rouge
+// bonne réponse id"res1-1" par exemple
+// mauvaise réponse : celle sélectionnée
+
+// function isTrue() {
+//   var valeur = "";
+//   for (let i = 0; i < inputList.length; i++) {
+//     if (inputList[i].checked) {
+//       valeur = inputList[i].value;
+//     }
+//     if (inputList[4].checked) {
+//       console.log("oui");
+//     }
+//   }
+//   console.log(valeur + " a été sélectionné");
+//   if (document.getElementById("res1-1").checked === true) {
+//     console.log("vrai");
+//   }
+// }
 
 // recuperer la valeur avec checked
 // retourn true si coché, false sinon
-document.getElementById("res1-1").checked;
+// document.getElementById("res1-1").checked;
 
 // pour traiter les données
-if (document.getElementById("res1-1").checked === true) {
-  //bla bla bla//
-  console.log("vrai");
-}
+// if (document.getElementById("res1-1").checked === true) {
+//bla bla bla//
+// }
